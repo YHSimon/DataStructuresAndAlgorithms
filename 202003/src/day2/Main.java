a25a7d65add964ea5c3dac6394a0b1bc9d9bd454
YHSimon <1981970494@qq.com>
2020-03-25 00:15:32 +0800

    跳台阶

--- 202003/src/day2/Main.java
+++ 202003/src/day2/Main.java
@@ -1,6 +1,10 @@
 package day2;
 
 public class Main {
+    public static void main(String[] args) {
+        Main m=new Main();
+        System.out.println(m.JumpFloor(5));
+    }
     public int minNumberInRotateArray(int [] array) {
         if(array.length==0){
             return 0;
@@ -32,4 +36,27 @@ public class Main {
         }
         return  res;
     }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+     *
+     * 递归思想
+     *
+     */
+    public int JumpFloor(int target) {
+        if(target <= 0) return 0;
+        if(target == 1) return 1;
+        if(target == 2) return 2;
+        int one = 1;
+        int two = 2;
+        int result = 0;
+        for(int i = 2; i < target; i++){
+            result = one+ two;
+            one = two;
+            two = result;
+        }
+        return result;
+    }
+
 }
