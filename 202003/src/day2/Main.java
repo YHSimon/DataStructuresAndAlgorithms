44de27690e5b953bc231cf73de0dc4608f71fa77
YHSimon <1981970494@qq.com>
2020-03-26 20:57:15 +0800

    变态跳台阶 基于普通跳台阶的进阶

--- 202003/src/day2/Main.java
+++ 202003/src/day2/Main.java
@@ -3,8 +3,62 @@ package day2;
 public class Main {
     public static void main(String[] args) {
         Main m=new Main();
-        System.out.println(m.JumpFloor(5));
+//        System.out.println(m.JumpFloor(5));
+
+        System.out.println(m.JumpFloorII(5));
     }
+
+    /**
+     *一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+     *
+     * 距离目标还剩n~1层，因而形成了n种方法，当然还有一种情况是从0层直接跳到目标，所以总方案数是（前n-1项和）+1
+     */
+    public int JumpFloorII(int target) {
+        if(target <=0){
+            return 0;
+        }
+        if(target==1){
+            return 1;
+        }
+        if(target==2){
+            return 2;
+        }
+        if(target==3){
+            return 4;
+        }
+        int sum=0;
+        //记录前n-1项和
+        int preSum=7;
+        for(int i=4;i<=target;i++){
+            sum=preSum+1;
+            preSum=preSum+sum;
+        }
+        return sum;
+    }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+     *
+     * 递归思想
+     *
+     */
+    public int JumpFloor(int target) {
+        if(target <= 0) return 0;
+        if(target == 1) return 1;
+        if(target == 2) return 2;
+        int one = 1;
+        int two = 2;
+        int result = 0;
+        for(int i = 2; i < target; i++){
+            result = one+ two;
+            one = two;
+            two = result;
+        }
+        return result;
+    }
+
+
     public int minNumberInRotateArray(int [] array) {
         if(array.length==0){
             return 0;
@@ -35,28 +89,6 @@ public class Main {
             a[1]=res;
         }
         return  res;
-    }
-
-    /**
-     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
-     * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
-     *
-     * 递归思想
-     *
-     */
-    public int JumpFloor(int target) {
-        if(target <= 0) return 0;
-        if(target == 1) return 1;
-        if(target == 2) return 2;
-        int one = 1;
-        int two = 2;
-        int result = 0;
-        for(int i = 2; i < target; i++){
-            result = one+ two;
-            one = two;
-            two = result;
-        }
-        return result;
     }
 
 }
