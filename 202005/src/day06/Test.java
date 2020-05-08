3654ce01240c955db0326a13c83f5887d8311f9c
YHSimon <1981970494@qq.com>
2020-05-08 18:48:47 +0800

    day06_最大正方形

--- 202005/src/day06/Test.java
+++ 202005/src/day06/Test.java
@@ -1,7 +1,7 @@
 package day06;
 
 /**题目：
- * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形， 并返回其面积。
  */
 public class Test {
     public static void main(String[] args) {
