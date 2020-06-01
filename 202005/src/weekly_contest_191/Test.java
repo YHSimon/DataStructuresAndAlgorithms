53e49a0d75937253563b4e23717127ba020ed318
YHSimon <1981970494@qq.com>
2020-06-01 20:25:52 +0800

    weekly_contest_191

--- 202005/src/weekly_contest_191/Test.java
+++ 202005/src/weekly_contest_191/Test.java
@@ -1,4 +1,18 @@
 package weekly_contest_191;
 
 public class Test {
+    public static void main(String[] args) {
+        Test t=new Test();
+        System.out.println(t.maxProduct(new int[]{1,5,4,5}));
+    }
+    public int maxProduct(int[] nums) {
+        int ans=Integer.MIN_VALUE;
+        int len=nums.length;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                ans=Math.max(ans, (nums[i]-1)*(nums[j]-1));
+            }
+        }
+        return ans;
+    }
 }
--- 202005/src/weekly_contest_191/Test2.java
+++ 202005/src/weekly_contest_191/Test2.java
@@ -1,4 +1,30 @@
 package weekly_contest_191;
 
+import java.util.Arrays;
+
 public class Test2 {
+    public static void main(String[] args) {
+        Test2 t=new Test2();
+        int[] horizontalCuts=new int[]{3};
+        int[] verticalCuts=new int[]{3};
+        System.out.println(t.maxArea(5,4,horizontalCuts,verticalCuts));
+    }
+
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int hLen = horizontalCuts.length, vLen = verticalCuts.length;
+        long hMax = horizontalCuts[0];
+        long vMax = verticalCuts[0];
+        for (int i = 1; i < hLen; i++) {
+            hMax = Math.max(hMax, horizontalCuts[i] - horizontalCuts[i - 1]);
+        }
+        for (int i = 1; i < vLen; i++) {
+            vMax = Math.max(vMax, verticalCuts[i] - verticalCuts[i - 1]);
+        }
+
+        hMax = Math.max(hMax, h - horizontalCuts[hLen - 1]);
+        vMax = Math.max(vMax, w - verticalCuts[vLen - 1]);
+       return (int)((hMax*vMax)%(1000000007));
+    }
 }
