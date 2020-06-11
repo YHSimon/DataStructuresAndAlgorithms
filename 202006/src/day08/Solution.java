08af3e023253fed3c32b57ff973e7ebf11f2a9dd
YHSimon <1981970494@qq.com>
2020-06-11 14:42:57 +0800

    day08_739_每日温度

---
+++ 202006/src/day08/Solution.java
@@ -0,0 +1,63 @@
+package day08;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+
+/**739.每日温度
+ *
+ * 根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+ *
+ * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ *
+ * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ */
+public class Solution {
+    //4ms
+    public int[] dailyTemperatures3(int[] T) {
+        int[] res = new int[T.length];
+        res[T.length - 1] = 0;
+        for (int i = T.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < T.length; j += res[j]) {
+                if (T[i] < T[j]) {
+                    res[i] = j - i;
+                } else if (res[j] == 0) {
+                    res[i] = 0;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    //单调栈  16ms
+    public int[] dailyTemperatures2(int[] T) {
+        if (T == null || T.length == 0) return new int[0];
+        int[] ans = new int[T.length];
+        int len = T.length;
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            int t = T[i];
+            while (!stack.isEmpty() && t > T[stack.peek()]) {
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    public int[] dailyTemperatures(int[] T) {
+        if (T == null || T.length == 0) return new int[0];
+        int[] ans = new int[T.length];
+        for (int i = 0; i < T.length; i++) {
+            for (int j = i + 1; j < T.length; j++) {
+                if (T[j] > T[i]) {
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
